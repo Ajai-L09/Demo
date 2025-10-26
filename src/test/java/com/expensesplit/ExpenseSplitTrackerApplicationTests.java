a51d717179e0cbfe38b80008cb4fcd34e20a6070
Ajai-L09 <ajai.l2024cse@sece.ac.in>
2025-10-26 10:09:16 +0530

    Add files via upload

---
+++ src/test/java/com/expensesplit/ExpenseSplitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensesplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
